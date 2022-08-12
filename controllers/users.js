6535d5ae9ae40c3897286dc5dd5d0e64646e136d
Vladimir Pavlov <mandran@inbox.ru>
2022-08-12 22:48:07 +0800

    Проект на проверку

--- controllers/users.js
+++ controllers/users.js
@@ -37,7 +37,7 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(201).send({ data: user }))
+    .then((user) => res.status(201).send({ user }))
     .catch((error) => {
       if(error.name === 'ValidationError') {
         res.status(400).send({ message: `Произошла ошибка при создании пользователя ${error}` })
