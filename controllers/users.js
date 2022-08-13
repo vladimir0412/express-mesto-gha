9443a996ff2fba14a7f141f20e2903ae369b6418
Vladimir Pavlov <mandran@inbox.ru>
2022-08-13 12:53:05 +0800

    Проект на проверку 5

--- controllers/users.js
+++ controllers/users.js
@@ -26,6 +26,8 @@ const getUserById = (req, res) => {
     .catch((error) => {
       if(error.name === 'UserNotFound') {
         res.status(error.status).send(error);
+      } else if (error.name === 'ValidationError') {
+        res.status(400).send({ message: `Произошла ошибка: некорректные данные id ${error}` });
       } else {
         res.status(500).send({ message: `Произошла ошибка сервера ${error}` });
       }
