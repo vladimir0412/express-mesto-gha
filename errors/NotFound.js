3db89f4fa07695c22809590628da600f6fd72bad
Vladimir Pavlov <mandran@inbox.ru>
2022-08-20 00:38:47 +0800

    Проект на проверку 20

--- errors/NotFound.js
+++ errors/NotFound.js
@@ -1,6 +1,5 @@
 const ApplicationError = require('./ApplicationError');
 
-
 module.exports = class NotFound extends ApplicationError {
   constructor() {
     super(404, 'Объект запроса не найден');
