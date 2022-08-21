d0334742e7d980b29cfbb58770fee2f140b49cbd
Vladimir Pavlov <mandran@inbox.ru>
2022-08-21 12:56:59 +0800

    Сдать проект на проверку

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,21 +1,24 @@
-const Card = require('../models/card');
-const NotFound = require('../errors/NotFound');
 const BadRequest = 400;
 const ServerError = 500;
+const NotFound = require('../errors/NotFound');
+const Card = require('../models/card');
 
 const getCards = (req, res) => {
-  const { name, link, owner, likes, createdAt } = req.body;
+  const {
+    name, link, owner, likes, createdAt,
+  } = req.body;
 
-  Card.find({ name, link, owner, likes, createdAt })
+  Card.find({
+    name, link, owner, likes, createdAt,
+  })
     .then((card) => res.send({ card }))
     .catch((error) => {
-      if(error.name === 'ValidationError') {
+      if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка при получении карточек' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const createCard = (req, res) => {
@@ -24,37 +27,34 @@ const createCard = (req, res) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(201).send({ card }))
     .catch((error) => {
-      if(error.name === 'ValidationError') {
+      if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка при создании карточки' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const deleteCardById = (req, res) => {
-
   Card.findByIdAndRemove(req.params.cardId)
     .orFail(() => {
       throw new NotFound();
     })
     .then((card) => res.send({ card }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'CastError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка: некорректные данные id' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const likeCard = (req, res) => {
-
-  Card.findByIdAndUpdate(req.params.cardId,
+  Card.findByIdAndUpdate(
+    req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
@@ -63,20 +63,19 @@ const likeCard = (req, res) => {
     })
     .then((card) => res.send({ card }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'CastError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка: некорректные данные id' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const dislikeCard = (req, res) => {
-
-  Card.findByIdAndUpdate(req.params.cardId,
+  Card.findByIdAndUpdate(
+    req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
@@ -85,15 +84,16 @@ const dislikeCard = (req, res) => {
     })
     .then((card) => res.send({ card }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'CastError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка: некорректные данные id' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
-module.exports = { getCards, createCard, deleteCardById, likeCard, dislikeCard };
+module.exports = {
+  getCards, createCard, deleteCardById, likeCard, dislikeCard,
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
-const User = require('../models/user');
 const NotFound = require('../errors/NotFound');
+const User = require('../models/user');
+
 const BadRequest = 400;
 const ServerError = 500;
 
@@ -9,32 +10,29 @@ const getUsers = (req, res) => {
   User.find({ name, about, avatar })
     .then((users) => res.send({ users }))
     .catch((error) => {
-      if(error.name === 'ValidationError') {
+      if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Запрашиваемый пользователь не найден' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const getUserById = (req, res) => {
-
   User.findById(req.params.userId)
     .orFail(() => {
       throw new NotFound();
     })
     .then((user) => res.send({ user }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'CastError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка: некорректные данные id' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const createUser = (req, res) => {
@@ -43,57 +41,52 @@ const createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send({ data: user }))
     .catch((error) => {
-      if(error.name === 'ValidationError') {
+      if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка при создании пользователя' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const editUser = (req, res) => {
   const { name, about } = req.body;
 
-  User.findByIdAndUpdate(req.user._id,
-    { name, about }, { new: true, runValidators: true },
-  )
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail(() => {
       throw new NotFound();
     })
     .then((user) => res.send({ user }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка при редактировании профиля' });
       } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
 const editAvatar = (req, res) => {
   const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.user._id,
-    { avatar }, { new: true, runValidators: true },
-  )
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .orFail(() => {
       throw new NotFound();
     })
     .then((user) => res.send({ user }))
     .catch((error) => {
-      if(error.name === 'NotFound') {
+      if (error.name === 'NotFound') {
         res.status(error.status).send(error);
       } else if (error.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Произошла ошибка при смене аватара' });
-      } else {;
+      } else {
         res.status(ServerError).send({ message: 'Произошла ошибка сервера' });
       }
-    }
-  );
+    });
 };
 
-module.exports = { getUsers, getUserById, createUser, editUser, editAvatar };
+module.exports = {
+  getUsers, getUserById, createUser, editUser, editAvatar,
+};
