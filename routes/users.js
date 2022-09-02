84739219dc7db4ebc4c74d3f461eedfe6f567a23
Vladimir Pavlov <mandran@inbox.ru>
2022-09-02 21:42:45 +0300

    for test

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router(); // создали роутер
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserById, getUserInfo, editUser, editAvatar,
 } = require('../controllers/users'); // данные нужны для роутинга, поэтому импортируем их
@@ -11,7 +12,12 @@ router.get('/users/me', getUserInfo);
 
 // router.post('/users', createUser);
 
-router.patch('/users/me', editUser);
+router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), editUser);
 
 router.patch('/users/me/avatar', editAvatar);
 
