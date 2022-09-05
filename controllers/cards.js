95b7fd12a86c32186a06696e0e830f4e6e0371a9
Vladimir Pavlov <mandran@inbox.ru>
2022-09-05 17:39:39 +0300

    test 2

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,7 +13,7 @@ const createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send({ card }))
+    .then((card) => res.send(card))
     .catch((error) => {
       if (error.name === 'CastError') {
         next(new BadRequest('Карточка не найдена.'));
